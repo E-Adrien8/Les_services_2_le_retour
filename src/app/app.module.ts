3615ee09624ba1c544dc2e9c7683fa4cf9f6ada9
ESCARMANT Adrien <92037454+E-Adrien8@users.noreply.github.com>
2022-10-12 12:04:52 +0200

    final commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
+import { HttpClientModule } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    CocktailListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/internal/Observable';
+import { Cocktail } from './models/cocktail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CocktailService {
+
+  constructor(public http: HttpClient) { }
+
+  getCocktails(): Observable<Cocktail[]> {
+    return this.http.get<Cocktail[]>("assets/cocktail.json")
+  }
+}
